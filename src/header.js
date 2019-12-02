3cdc117fea36c59ce17ef58ee74075676c8949ba
danielmitiku <wedanielmitiku@gmail.com>
2019-12-02 17:03:00 +0300

    finished the requirements

---
+++ src/header.js
@@ -0,0 +1,28 @@
+
+const mainHeader = () => {
+    const topHeader = document.createElement('h1');
+    topHeader.textContent = 'Restaurant Page';
+    topHeader.classList.add('text-white')
+    return topHeader;
+}
+
+const navigation = () => {
+    const ul = document.createElement('ul');
+    const lists = ['Home', 'Menu', 'Contact'];
+    ul.classList.add('nav', 'nav-tabs');
+    for (let i = 0; i < 3; i++) {
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        const link = document.createElement('a');
+        link.textContent = lists[i];
+        link.setAttribute('id', lists[i]);
+        link.setAttribute('href', '#');
+        link.classList.add('nav-link');
+        li.appendChild(link);
+        ul.appendChild(li);
+    }
+    return ul;
+}
+
+export { mainHeader, navigation };
+
